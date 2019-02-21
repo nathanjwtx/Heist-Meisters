f34f0a5f1f54d9ccd2ea8dd43db4fc6b4dfeca9d
Nathan <nathanjwtx@nathanjwtx.io>
2019-02-20 20:07:16 -0600

    Experimenting with Tilemaps

---
+++ scenes/characters/Character.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+var SPEED : int = 10;
+var MAX_SPEED : int = 200;
+var FRICTION : float = 0.1;
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- scenes/characters/Player.gd
+++ scenes/characters/Player.gd
@@ -1,19 +1,29 @@
-extends Character
+extends "res://scenes/characters/Character.gd"
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-Vector2 MOTION = Vector2();
+var motion : Vector2 = Vector2();
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta : float):
 	update_motion(delta);
-	move_and_slide(MOTION);
+	move_and_slide(motion);
 
-func update_motion(float delta):
-	if input.is_action_pressed:
-		print("up");
+func update_motion(delta : float):
+	look_at(get_global_mouse_position());
+	
+	if Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_down"):
+		motion.y = clamp((motion.y - SPEED), - MAX_SPEED, 0);
+	elif Input.is_action_pressed("ui_down") and not Input.is_action_pressed("ui_up"):
+		motion.y = clamp((motion.y + SPEED), MAX_SPEED, 0);
+	else:
+		motion.y = lerp(motion.y, 0, FRICTION);
+		
+	if Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
+		motion.x = clamp((motion.x - SPEED), - MAX_SPEED, 0);
+	elif Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
+		motion.x = clamp((motion.x + SPEED), MAX_SPEED, 0);
+	else:
+		motion.x = lerp(motion.x, 0, FRICTION);
